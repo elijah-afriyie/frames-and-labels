6b8992a24a4b20ed11b922a9353c2e1e67491bfe
Elijah Afriyie <xlciie@xfraga>
2024-07-17 09:51:27 +0000

    commit deleted Main.class file and updated Main.java file

--- simple-button/src/MyFrame.java
+++ simple-button/src/MyFrame.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -5,7 +7,7 @@ import javax.swing.JFrame;
 /**
  * MyFrame
  */
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
 
   JButton button;
 
@@ -15,7 +17,8 @@ public class MyFrame extends JFrame {
     button.setBounds(200, 100, 100, 50);
     button.setText("Click Me");
     button.setIcon(icon);
-    button.addActionListener(e -> System.out.println("You clicked on a button, Bro!"));
+    button.setFocusable(false);
+    button.addActionListener(this);
 
     this.setTitle("My Awesome Title");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,5 +28,12 @@ public class MyFrame extends JFrame {
     this.setVisible(true);
 
     this.add(button);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == button) {
+      System.out.println("Button clicked!");
+    }
   }
 }
